c373a146e920274a5353e8267829bd81496ffc09
Christian DeJong <97701941+cdejong1@users.noreply.github.com>
2024-04-22 17:23:47 -0500

    Team and About Pages New Features
    Additional color formatting done to about page, team page header formatting completed with picture sizing adjustments.

--- php/team_page.php
+++ php/team_page.php
@@ -22,10 +22,65 @@
         </nav>
 
         <section>
-            <div> 
+            <div class = "members_header"> 
                 <h2>Student Leadership</h2>
                 <h3>Meet Our Team of Dedicated Student Leaders</h3>
             </div>
+
+
+        <!-- Team Member Sections -->
+        <div class="column">
+            <div class="row">
+                <img src="../resources/grason_randall.jpg" alt="grason randall" class="pic">
+                <div class="text-container"> 
+                    <p1 class="name"> Grason Randall, Treasurer </p1> <br />
+                    <p class="description">Grason is a Finance major at Concordia College Offutt School of Business in his senior year. He joined the fund in 2022 and is now the Treasurer of the fund. Along with other members, Grason leads club discussions, focusing on financial modeling and fundamental pricing. Currently, he is an intern at Legendary Capital in Fargo, ND. Legendary Capital is a private equity firm specializing in upscale and upper midscale hotels in the United States. Outside of the fund, Grason enjoys skiing, fishing, hiking, camping, and going to Micks.</p>
+                    <a href="https://www.linkedin.com/in/grason-randall-595646226/" target="_blank" class="linkedin-link">
+                        <img src="../resources/linkedin_logo.jpg" alt="LinkedIn" class="linkedin-logo">
+                    </a>
+                </div>
+            </div>
+            <div class="row">
+                <img src="../resources/kevin_ness.jpg" alt="kevin ness" class="pic">
+                <div class="text-container"> 
+                    <p1 class="name"> Kevin Ness, Analyst </p1> <br />
+                    <p class="description">Grason is a Finance major at Concordia College Offutt School of Business in his senior year. He joined the fund in 2022 and is now the Treasurer of the fund. Along with other members, Grason leads club discussions, focusing on financial modeling and fundamental pricing. Currently, he is an intern at Legendary Capital in Fargo, ND. Legendary Capital is a private equity firm specializing in upscale and upper midscale hotels in the United States. Outside of the fund, Grason enjoys skiing, fishing, hiking, camping, and going to Micks.</p>
+                    <a href="https://www.linkedin.com/in/kevinmness/" target="_blank" class="linkedin-link">
+                        <img src="../resources/linkedin_logo.jpg" alt="LinkedIn" class="linkedin-logo">
+                    </a>
+                </div>
+            </div>
+            <div class="row">
+                <img src="../resources/dylan_hinners.jpg" alt="dylan hinners" class="pic">
+                <div class="text-container"> 
+                    <p1 class="name"> Dylan Hinners, Analyst </p1> <br />
+                    <p class="description">Grason is a Finance major at Concordia College Offutt School of Business in his senior year. He joined the fund in 2022 and is now the Treasurer of the fund. Along with other members, Grason leads club discussions, focusing on financial modeling and fundamental pricing. Currently, he is an intern at Legendary Capital in Fargo, ND. Legendary Capital is a private equity firm specializing in upscale and upper midscale hotels in the United States. Outside of the fund, Grason enjoys skiing, fishing, hiking, camping, and going to Micks.</p>
+                    <a href="https://www.linkedin.com/in/dylan-hinners-8b3a3821a/" target="_blank" class="linkedin-link">
+                        <img src="../resources/linkedin_logo.jpg" alt="LinkedIn" class="linkedin-logo">
+                    </a>
+                </div>
+            </div>
+            <div class="row">
+                <img src="../resources/christian_dejong.jpg" alt="christian dejong" class="pic">
+                <div class="text-container"> 
+                    <p1 class="name"> Christian DeJong, Analyst </p1> <br />
+                    <p class="description">Grason is a Finance major at Concordia College Offutt School of Business in his senior year. He joined the fund in 2022 and is now the Treasurer of the fund. Along with other members, Grason leads club discussions, focusing on financial modeling and fundamental pricing. Currently, he is an intern at Legendary Capital in Fargo, ND. Legendary Capital is a private equity firm specializing in upscale and upper midscale hotels in the United States. Outside of the fund, Grason enjoys skiing, fishing, hiking, camping, and going to Micks.</p>
+                    <a href="https://www.linkedin.com/in/christian-dejong-b1333a210/" target="_blank" class="linkedin-link">
+                        <img src="../resources/linkedin_logo.jpg" alt="LinkedIn" class="linkedin-logo">
+                    </a>
+                </div>
+            </div>
+            <div class="row">
+                <img src="../resources/gabe_bellefuele.jpg" alt="gabe bellefeuille" class="pic">
+                <div class="text-container"> 
+                    <p1 class="name"> Gabe Bellefeuille, Analyst </p1> <br />
+                    <p class="description">Grason is a Finance major at Concordia College Offutt School of Business in his senior year. He joined the fund in 2022 and is now the Treasurer of the fund. Along with other members, Grason leads club discussions, focusing on financial modeling and fundamental pricing. Currently, he is an intern at Legendary Capital in Fargo, ND. Legendary Capital is a private equity firm specializing in upscale and upper midscale hotels in the United States. Outside of the fund, Grason enjoys skiing, fishing, hiking, camping, and going to Micks.</p>
+                    <a href="https://www.linkedin.com/in/gabe-bellefeuille/" target="_blank" class="linkedin-link">
+                        <img src="../resources/linkedin_logo.jpg" alt="LinkedIn" class="linkedin-logo">
+                    </a>
+                </div>
+            </div>
+        </div>
 
         </section>
 
